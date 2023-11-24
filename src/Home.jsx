bca7222e11d46d41d85a513b682f59f9b4d042af
Roman <roman.lukyanenko.rl@gmail.com>
2023-11-25 01:26:25 +0200

    react-36

--- src/Home.jsx
+++
@@ -1,8 +0,0 @@
-import Button, { Click } from "./components/Button"
-
-export const Home = () => (
-    <div>
-        <Button variant="primary" color="white" size="large" onClick={Click}> Основна кнопка </Button>
-        <Button variant="gray" color="black" size="medium" disabled> Неактивна кнопка </Button>
-    </div>
-);
