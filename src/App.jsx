f8d1581c30365da8989f00480a18f8a0fd7c5a4d
Roman <roman.lukyanenko.rl@gmail.com>
2023-11-26 06:30:07 +0200

    Test

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,13 @@
 import { useState } from 'react'
-import {ToDoList} from "./sections/toDoList/index"
 
 import './App.css'
 
 
 export const App = () => {
     
-    const [hiding, setHide] = useState(false);
 
-    const showText = () => {
-        setHide(!hiding);
-    }
     return (
         <>
-            <button onClick={showText}>
-                {hiding ? 'Show' : 'Hide'} Text
-            </button>
-            { !hiding && <h1>Show text</h1> }
-
-            <ToDoList />
 
         </>
     );
