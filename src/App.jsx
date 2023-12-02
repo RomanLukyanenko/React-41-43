46f1ed4aa42fc7489796658a2fb9df09e18d2501
Roman <roman.lukyanenko.rl@gmail.com>
2023-12-02 09:53:53 +0200

    react-38

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,11 @@
 import './App.css'
-import { Button } from './components/Button';
-import { Timer } from './components/Timer';
-import { Fetch } from './components/FetchPosts';
+
 
 
 
 export const App = () => {
     return (
         <>
-        <Button />
-        <Timer />
-        <Fetch />
         </>
         );
     };
