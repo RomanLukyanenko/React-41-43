76feb4bc93bfe2a5c110c399da411bcc67a3d171
Roman <roman.lukyanenko.rl@gmail.com>
2023-12-13 20:02:21 +0200

    react-41

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,4 @@
-import { useRef, useState } from "react";
-import { Button } from "./components/Button";
-
 
 export const App = () => {
-    const inputRef = useRef(null);
-    const [color, setColor] = useState()
-
-    return (
-        <>
-            <button onClick={() => {
-            setColor(inputRef.current.value)
-            }}>
-                Get color 
-            </button> 
-
-            <Button ref={inputRef}/>
-            
-            <p> Selected color {color} </p>
-        </>
-        );
+    return 
 };
--- src/components/Button.jsx
+++
@@ -1,7 +0,0 @@
-import { forwardRef } from "react"
-
-export const Button = forwardRef(function Button (props, ref) {
-    return (
-        <input type="color" ref={ref} ></input>
-    )
-}) 
