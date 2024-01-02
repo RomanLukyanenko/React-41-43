22b6dc35637a687f345498948e1664d163a849e7
Roman <roman.lukyanenko.rl@gmail.com>
2024-01-02 23:54:01 +0200

    for versel

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,49 @@
-import { Formik } from "formik";
-
+import React from 'react';
+import { useForm } from 'react-hook-form';
+import * as Yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
 
 export const App = () => {
-return (
+
+  const validationSchema = Yup.object().shape({
+    firstName: Yup.string().required("First Name is required"),
+    lastName: Yup.string().required("Last Name is required"),
+    city: Yup.string().required("City is required"),
+  });
+
+  const { register, handleSubmit, reset, formState: { errors } } = useForm({
+    resolver: yupResolver(validationSchema)
+  });
+
+  const onSubmit = (data) => {
+    console.log(data);
+    reset(); 
+  };
+
+  return (
     <>
-    <p> Formic </p>
-    <Formik
-    initialValues={{ firstName: "", lastName: "", city: "" }}
-    enableReinitialize
-    onSubmit={(values, formikBag ) => {
-        console.log(values);
-        console.log(formikBag);
-    }}
-    
-    >
-        {(...rest) => {
-        const { handleSubmit, handleChange, handleBlur, values } = rest;
-        console.log(rest);
-        return(
-            <form onSubmit={handleSubmit}>
-                <label>
-                 First Name:
-                <input 
-                name= "firstName" 
-                onChange={handleChange} 
-                onBlur={handleBlur} 
-                value={values.firstName}
-                />
-                </label>
-                <label>
-                Last Name: 
-                <input 
-                name= "lastName" 
-                onChange={handleChange} 
-                onBlur={handleBlur} 
-                value={values.lastName}
-                />
-                </label>
-                <label>
-                City:
-                 <input 
-                name= "city" 
-                onChange={handleChange} 
-                onBlur={handleBlur} 
-                value={values.city}
-                />
-                </label>
-                <button type="submit"> Submit </button>
-            </form>
-            )
-        }}
-    </Formik>
+      <p>React Hook Form</p>
+      <form onSubmit={handleSubmit(onSubmit)} style={{ display: "flex", flexDirection: "column", gap: "20px" }}>
+        <label>
+          First Name:
+          <input {...register('firstName')} />
+          {errors.firstName && <div style={{ color: "red" }}>{errors.firstName.message}</div>}
+        </label>
+
+        <label>
+          Last Name:
+          <input {...register('lastName')} />
+          {errors.lastName && <div style={{ color: "red" }}>{errors.lastName.message}</div>}
+        </label>
+
+        <label>
+          City:
+          <input {...register('city')} />
+          {errors.city && <div style={{ color: "red" }}>{errors.city.message}</div>}
+        </label>
+
+        <button type="submit" style={{ maxWidth: "100%", width: "15%" }}>Submit</button>
+      </form>
     </>
-    );
-}
+  );
+};
