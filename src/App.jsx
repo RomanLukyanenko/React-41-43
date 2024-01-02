6eef72e7300f3c44a7bb9fccae4615bb567cbc8c
Roman <roman.lukyanenko.rl@gmail.com>
2024-01-02 20:38:59 +0200

    rewe

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,55 @@
+import { Formik } from "formik";
 
 
 export const App = () => {
-};
+return (
+    <>
+    <p> Formic </p>
+    <Formik
+    initialValues={{ firstName: "", lastName: "", city: "" }}
+    enableReinitialize
+    onSubmit={(values, formikBag ) => {
+        console.log(values);
+        console.log(formikBag);
+    }}
+    >
+        {(...rest) => {
+        const { handleSubmit, handleChange, handleBlur, values } = rest;
+        console.log(rest);
+        return(
+            <form onSubmit={handleSubmit}>
+                <label>
+                 First Name:
+                <input 
+                name= "firstName" 
+                onChange={handleChange} 
+                onBlur={handleBlur} 
+                value={values.firstName}
+                />
+                </label>
+                <label>
+                Last Name: 
+                <input 
+                name= "lastName" 
+                onChange={handleChange} 
+                onBlur={handleBlur} 
+                value={values.lastName}
+                />
+                </label>
+                <label>
+                City:
+                 <input 
+                name= "city" 
+                onChange={handleChange} 
+                onBlur={handleBlur} 
+                value={values.city}
+                />
+                </label>
+                <button type="submit"> Submit </button>
+            </form>
+            )
+        }}
+    </Formik>
+    </>
+    );
+}
