d71483962c8df7253626d3f6da5a3b1610ac4b7e
Roman <roman.lukyanenko.rl@gmail.com>
2024-01-08 18:52:41 +0200

    good

---
+++ src/components/CartNoResult.jsx
@@ -0,0 +1,10 @@
+const CartNoResult = () => {
+    return (
+        <span className="no-result no-result--inline">
+            <img src="../../public/no-result/no-result-v2.png" alt="No results" className="no-result__img" />
+            <span className="no-result__title">Результатів не знайдено</span>
+          </span>
+        );
+};
+
+export default CartNoResult;
--- src/sections/Cart.jsx
+++ src/sections/Cart.jsx
@@ -1,12 +1,22 @@
-import { useCallback } from 'react';
+import React, { useCallback } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { removeFromCart, increaseItemCount, decreaseItemCount, changeCartVisibility } from '../store/CartSlice';
+import {
+  removeFromCart,
+  increaseItemCount,
+  decreaseItemCount,
+  changeCartVisibility
+} from '../store/CartSlice';
+import CartNoResult from '../components/CartNoResult';
+import CartItem from '../components/CartItem';
+import CartButton from '../components/CartButton';
+import config from '../store/config'; // Перевірте правильність шляху
 
 export const Cart = () => {
   const dispatch = useDispatch();
-
+  
   // Отримання даних корзини з Redux store
   const { cart, cartVisibility } = useSelector(state => state.cart);
+  const cartCount = cart.length;
 
   // Видалення товару з корзини
   const delCartProduct = useCallback((productId) => {
@@ -24,50 +34,32 @@ export const Cart = () => {
   }, [dispatch]);
 
   // Перемикач видимості корзини
-  const toggleCartVisibility = useCallback(() => {
+  const toggleCart = useCallback(() => {
     dispatch(changeCartVisibility());
   }, [dispatch]);
 
   return (
     <div className="cart-added-list">
-      <button className="cart-added-list__btn" onClick={toggleCartVisibility}>
-        {/* Іконка та кількість товарів у кошику */}
-        <span className="cart-added-summ js-cart-added-summ">{cart.length}</span>
-        <svg className="icon icon-cart-bag">
-          <use xlinkHref="#icon-cart-bag"></use>
-        </svg>
-      </button>
-
-      {cartVisibility && (
-        cart.length === 0 ? (
-          <span className="no-result no-result--inline">
-            {/* Повідомлення про порожню корзину */}
-            <img src="../../public/no-result/no-result-v2.png" alt="No results" className="no-result__img" />
-            <span className="no-result__title">Результатів не знайдено</span>
-          </span>
+      <CartButton cartCount={cartCount} toggleCart={toggleCart} />
+      <div className={`cart-added-list__item-list ${cartVisibility ? 'show' : ''}`}>
+        {cart.length === 0 ? (
+          <CartNoResult />
         ) : (
-          cart.map((product) => (
-            <div key={product.id} className="cart-added-list__item">
-              {/* Вміст товару у кошику */}
-              <button className="cart-added-list__item-btn-delete btn-light" onClick={() => delCartProduct(product.id)}>
-                <svg className='icon icon-close'>
-                  <use xlinkHref='#icon-close'></use>
-                </svg>
-              </button>
-              <img src={`../../public/catalog/${product.img}`} alt={product.name} className="cart-added-list__item-img" />
-              <p className="cart-added-list__item-text-hold">
-                <span className="cart-added-list__item-title-link">{product.title}</span>
-                <span className="cart-added-list__item-meta-list">
-                  <span className="cart-added-list__item-meta">Ціна: {product.price} грн</span>
-                </span>
-              </p>
-              <input type="text" className="cart-added-list__item-count" value={product.count} readOnly/>
-              <button className="cart-added-list__item-btn-plus btn-light" onClick={() => setItemCountPlus(product.id)}></button>
-              <button className="cart-added-list__item-btn-minus btn-light" onClick={() => setItemCountMinus(product.id)}></button>
-            </div>
-          ))
-        )
-      )}
+          cart.map((product, index) => {
+            const cartItemProps = {
+              key: index,
+              pathImg: config.pathImg,
+              product,
+              delCartProduct,
+              setItemCountPlus,
+              setItemCountMinus,
+            };
+            return <CartItem {...cartItemProps} />;
+          })
+        )}
+      </div>
     </div>
   );
 };
+
+export default Cart;
